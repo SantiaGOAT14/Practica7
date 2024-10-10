818049ae3cce9cfb4eb08d791ba60fac85b61a78
SantiaGOAT14 <136783629+SantiaGOAT14@users.noreply.github.com>
2024-10-09 22:43:11 -0600

    Update Ejercicio2.c

--- Ejercicio2.c
+++ Ejercicio2.c
@@ -21,4 +21,31 @@ int main ()
     }
     return 0;
 }
-      
+//Este fue el desarrollado en clase, sin embargo en casa cambié un poco el programa para que fuera cierto
+    // Se deternuba el orden de los números
+    if (uno > dos && uno > tres) {
+        // uno es mayor
+        if (dos > tres) {
+            printf("%d es mayor que %d, que es mayor que %d\n", uno, dos, tres);
+        } else {
+            printf("%d es mayor que %d, que es mayor que %d\n", uno, tres, dos);
+        }
+    } else if (dos > uno && dos > tres) {
+        // dos es mayor
+        if (uno > tres) {
+            printf("%d es mayor que %d, que es mayor que %d\n", dos, uno, tres);
+        } else {
+            printf("%d es mayor que %d, que es mayor que %d\n", dos, tres, uno);
+        }
+    } else {
+        // tres es mayor
+        if (uno > dos) {
+            printf("%d es mayor que %d, que es mayor que %d\n", tres, uno, dos);
+        } else {
+            printf("%d es mayor que %d, que es mayor que %d\n", tres, dos, uno);
+        }
+    }
+
+    return 0;
+}
+
